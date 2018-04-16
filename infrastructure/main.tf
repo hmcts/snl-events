aba8a7c52cfd040d53c9f12e9ba260d8f7218697
Vijaya raghava Krishna shetty <vijayrkshetty@gmail.com>
2018-04-16 10:32:06 +0100

    typo fixed

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,16 +12,16 @@ module "snl-events" {
   additional_host_name = "${var.external_host_name}"
 
   app_settings = {
-    DEFINITION_STORE_DB_HOST = "${module.postgres-sln-events.host_name}"
-    DEFINITION_STORE_DB_PORT = "${module.postgres-sln-events.postgresql_listen_port}"
-    DEFINITION_STORE_DB_NAME = "${module.postgres-sln-events.postgresql_database}"
-    DEFINITION_STORE_DB_USERNAME = "${module.postgres-sln-events.user_name}"
-    DEFINITION_STORE_DB_PASSWORD = "${module.postgres-sln-events.postgresql_password}"
+    SNL-EVENTS_DB_HOST = "${module.postgres-snl-events.host_name}"
+    SNL-EVENTS_DB_PORT = "${module.postgres-snl-events.postgresql_listen_port}"
+    SNL-EVENTS_DB_NAME = "${module.postgres-snl-events.postgresql_database}"
+    SNL-EVENTS_DB_USERNAME = "${module.postgres-snl-events.user_name}"
+    SNL-EVENTS_DB_PASSWORD = "${module.postgres-snl-events.postgresql_password}"
   }
 
 }
 
-module "postgres-sln-events" {
+module "postgres-snl-events" {
   source              = "git@github.com:contino/moj-module-postgres?ref=master"
   product             = "${var.product}-${var.component}"
   location            = "West Europe"
