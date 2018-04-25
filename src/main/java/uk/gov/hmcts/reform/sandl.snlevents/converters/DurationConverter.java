18e5ab8fda665f68e558fa36388e6923c93aa9de
Remigiusz Kruk <R.Kruk@kainos.com>
2018-04-25 14:35:26 +0200

    remove unused logger

--- src/main/java/uk/gov/hmcts/reform/sandl.snlevents/converters/DurationConverter.java
+++ src/main/java/uk/gov/hmcts/reform/sandl.snlevents/converters/DurationConverter.java
@@ -4,13 +4,9 @@ import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.logging.Logger;
 
 @Converter(autoApply = true)
 public class DurationConverter implements AttributeConverter<Duration, Long> {
-
-    Logger log = Logger.getLogger(DurationConverter.class.getSimpleName());
-
     @Override
     public Long convertToDatabaseColumn(Duration attribute) {
         return attribute.getSeconds();
