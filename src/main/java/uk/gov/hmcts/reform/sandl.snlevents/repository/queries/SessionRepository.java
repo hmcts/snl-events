e1857a80b0de4e2e73d803a634373a85a465be61
radoslawlandowskikainos <r.landowski@kainos.com>
2018-04-27 17:47:19 +0200

    Fix class reference

--- src/main/java/uk/gov/hmcts/reform/sandl.snlevents/repository/queries/SessionRepository.java
+++ src/main/java/uk/gov/hmcts/reform/sandl.snlevents/repository/queries/SessionRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SessionRepository {
-    String GET_SESSION_INFO_SQL = "SELECT NEW uk.gov.hmcts.reform.sandl.snlevents.model."
+    String GET_SESSION_INFO_SQL = "SELECT NEW uk.gov.hmcts.reform.sandl.snlevents.model.response."
         + "SessionInfo(s.id, s.start, s.duration, p, r) "
         + "FROM Session s, Person p, Room r WHERE s.start BETWEEN :dateStart AND :dateEnd "
         + "AND p.personType = 'JUDGE'";
