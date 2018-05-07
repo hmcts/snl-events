28470e9b399543391cbc21ff7e67a4346112db32
Remigiusz Kruk <R.Kruk@kainos.com>
2018-05-07 16:06:31 +0200

    change query

--- src/main/java/uk/gov/hmcts/reform/sandl.snlevents/repository/queries/SessionRepository.java
+++ src/main/java/uk/gov/hmcts/reform/sandl.snlevents/repository/queries/SessionRepository.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SessionRepository {
+
     String GET_SESSION_INFO_SQL = "SELECT NEW uk.gov.hmcts.reform.sandl.snlevents.model.response."
         + "SessionInfo(s.id, s.start, s.duration, p, r) "
-        + "FROM Session s, Person p, Room r WHERE s.start BETWEEN :dateStart AND :dateEnd "
-        + "AND p.personType = 'JUDGE'";
+        + "FROM Session s LEFT OUTER JOIN s.person as p LEFT OUTER JOIN s.room as r "
+        + "WHERE s.start BETWEEN :dateStart AND :dateEnd "
+        + "AND (p.personType = 'JUDGE' or p is null)";
 
     String GET_SESSION_FOR_JUDGE_DIARY_SQL = "SELECT NEW uk.gov.hmcts.reform.sandl.snlevents.model.response."
         + "SessionInfo(s.id, s.start, s.duration, p, r) "
