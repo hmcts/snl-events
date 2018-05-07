99e07d0256ef789707eac9016d48bc5ca4b5bdce
radoslawlandowskikainos <r.landowski@kainos.com>
2018-05-07 16:49:57 +0200

    Fix query for getting sessions

--- src/main/java/uk/gov/hmcts/reform/sandl.snlevents/repository/queries/SessionRepository.java
+++ src/main/java/uk/gov/hmcts/reform/sandl.snlevents/repository/queries/SessionRepository.java
@@ -14,6 +14,7 @@ public interface SessionRepository {
 
     String GET_SESSION_FOR_JUDGE_DIARY_SQL = "SELECT NEW uk.gov.hmcts.reform.sandl.snlevents.model.response."
         + "SessionInfo(s.id, s.start, s.duration, p, r) "
-        + "FROM Session s, Person p, Room r WHERE s.start BETWEEN :dateStart AND :dateEnd "
+        + "FROM Session s LEFT OUTER JOIN s.person as p LEFT OUTER JOIN s.room as r "
+        + "WHERE s.start BETWEEN :dateStart AND :dateEnd "
         + "AND p.personType = 'JUDGE' AND p.username = :judgeUsername";
 }
