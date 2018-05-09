8d3de577dc57f47a671936e37c21610e36fe4faf
Remigiusz Kruk <R.Kruk@kainos.com>
2018-05-09 15:11:55 +0200

    add endpoint for assigning hearing with session

--- src/main/java/uk/gov/hmcts/reform/sandl.snlevents/service/HearingPartService.java
+++ src/main/java/uk/gov/hmcts/reform/sandl.snlevents/service/HearingPartService.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.reform.sandl.snlevents.service;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.sandl.snlevents.model.db.HearingPart;
-import uk.gov.hmcts.reform.sandl.snlevents.repository.db.HearingPartRepository;
 
-import java.util.List;
+import uk.gov.hmcts.reform.sandl.snlevents.model.db.HearingPart;
+import uk.gov.hmcts.reform.sandl.snlevents.model.db.Session;
+import uk.gov.hmcts.reform.sandl.snlevents.repository.db.HearingPartRepository;
+import uk.gov.hmcts.reform.sandl.snlevents.repository.db.SessionRepository;
 
 @Service
 public class HearingPartService {
@@ -13,11 +17,24 @@ public class HearingPartService {
     @Autowired
     HearingPartRepository hearingPartRepository;
 
+    @Autowired
+    SessionRepository sessionRepository;
+
     public List<HearingPart> getAllHearingParts() {
         return hearingPartRepository.findAll();
     }
 
     public HearingPart save(HearingPart hearingPart) {
+        return hearingPartRepository.save(hearingPart);
+    }
+
+    public HearingPart assignHearingPartWithSession(UUID hearingPartId, UUID sessionId) {
+        HearingPart hearingPart = hearingPartRepository.findOne(hearingPartId);
+
+        Session session = sessionRepository.findOne(sessionId);
+
+        hearingPart.setSession(session);
+
         return hearingPartRepository.save(hearingPart);
     }
 }
