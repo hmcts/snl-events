51c8d4cfa6bda018ddd11db5bf78977a639574f7
Remigiusz Kruk <R.Kruk@kainos.com>
2018-05-10 10:03:07 +0200

    add sample hearings

---
+++ src/main/resources/db/fixtures/SL-907-add-sample-hearings.sql
@@ -0,0 +1,9 @@
+INSERT INTO hearing_part VALUES
+('93cfbe86-ceff-40a8-94f2-3ba2155f22a9','1234','case title','FTRACK','hearing type',3600,'2018-05-02','2018-06-02',NULL,NULL);
+INSERT INTO hearing_part VALUES
+('93cfbe86-ceff-40a8-94f2-3ba2155f22a8','1234','case title','SCLAIMS','hearing type',3600,'2018-05-02','2018-06-02',NULL,NULL);
+INSERT INTO hearing_part VALUES
+('93cfbe86-ceff-40a8-94f2-3ba2155f22a7','1234','case title','MTRACK','hearing type 1',1800,'2018-05-02','2018-06-02',NULL,NULL);
+INSERT INTO hearing_part VALUES
+('93cfbe86-ceff-40a8-94f2-3ba2155f22a6','1234','case title','FTRACK','hearing type 2',900,'2018-03-02','2018-06-02',
+'dfba5778-137c-fad0-734e-6c7bddcbf586', '2018-04-25 11:30:00');
