3d1f348eeb5e09a5f3caa226f776e6d04483665c
Remigiusz Kruk <R.Kruk@kainos.com>
2018-05-15 11:37:12 +0200

    chekstyle fix

--- src/main/java/uk/gov/hmcts/reform/sandl.snlevents/controllers/ProblemController.java
+++ src/main/java/uk/gov/hmcts/reform/sandl.snlevents/controllers/ProblemController.java
@@ -1,16 +1,15 @@
 package uk.gov.hmcts.reform.sandl.snlevents.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import uk.gov.hmcts.reform.sandl.snlevents.model.db.Problem;
 import uk.gov.hmcts.reform.sandl.snlevents.service.ProblemService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/problems")
--- src/main/java/uk/gov/hmcts/reform/sandl.snlevents/service/ProblemService.java
+++ src/main/java/uk/gov/hmcts/reform/sandl.snlevents/service/ProblemService.java
@@ -1,11 +1,12 @@
 package uk.gov.hmcts.reform.sandl.snlevents.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sandl.snlevents.model.db.Problem;
 import uk.gov.hmcts.reform.sandl.snlevents.repository.db.ProblemRepository;
+
+import java.util.List;
+
 
 @Service
 public class ProblemService {
