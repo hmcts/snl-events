49685449f5bcd4734581135e1e3d169143ef8a3a
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2018-05-24 12:04:29 +0200

    SL-871 checkstyle and pmd update

--- src/main/java/uk/gov/hmcts/reform/sandl.snlevents/model/db/Problem.java
+++ src/main/java/uk/gov/hmcts/reform/sandl.snlevents/model/db/Problem.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import java.util.List;
+
 
 @Entity
 @Data
