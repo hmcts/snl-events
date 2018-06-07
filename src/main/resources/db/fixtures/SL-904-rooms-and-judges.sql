3e937c6c3aff6ac4e35a49501ee65a477e66f425
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2018-06-07 18:09:15 +0200

    SL-904 New rooms and judges

---
+++ src/main/resources/db/fixtures/SL-904-rooms-and-judges.sql
@@ -0,0 +1,16 @@
+-- rooms
+insert into room values ('dc4ce4dc-b24f-4b46-91ab-014d4a28469b', 'Court 1');
+insert into room values ('8d269688-064c-41b6-a0a6-8ae4033a23e1', 'Court 2');
+insert into room values ('c73df533-f067-4535-935c-d798c833ea33', 'Court 3');
+insert into room values ('ed613c62-ea50-4de5-ad9a-07f46dac3f11', 'Court 4');
+insert into room values ('fb878f53-a2dc-4cfc-aac1-ca1a1c697a8e', 'Court 5');
+insert into room values ('c8c5554c-cd8d-42ca-8503-7eae334e255e', 'Court 6');
+insert into room values ('b08d0570-1759-412d-8141-d8e1fd46de30', 'Court 7');
+
+-- judges
+insert into person values ('6ce3aa58-6986-4ed7-8c61-94fa2af73afb', 'JUDGE', 'DJ Cope', 'djcope');
+insert into person values ('a537cc0e-c275-479d-a744-44c782ec7d04', 'JUDGE', 'DJ Cronin', 'djcronin');
+insert into person values ('d5effa95-877f-4eaf-a029-8aaa2d088289', 'JUDGE', 'DJ Howell', 'djhowell');
+insert into person values ('0010fa96-22e6-4d56-98da-29adb125b246', 'JUDGE', 'DJ Michaels', 'djmichaels');
+insert into person values ('d85450a9-4925-40d1-b6ae-918e0f8741a7', 'JUDGE', 'DJ Smith', 'djsmith');
+insert into person values ('a70f3975-379c-431c-b999-d96cba7d2b4a', 'JUDGE', 'DJ Watson', 'djwatson');
