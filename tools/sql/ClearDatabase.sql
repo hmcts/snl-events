fa8694bb4a31dd6db8c20ca31e2f277a218cd5cc
Kamil Pakur <k.pakur@kainos.com>
2018-06-15 16:55:26 +0200

    added scripts to generate some data

---
+++ tools/sql/ClearDatabase.sql
@@ -0,0 +1,9 @@
+delete from user_transaction_data;
+delete from user_transaction;
+delete from problem_reference;
+delete from problem;
+delete from hearing_part;
+delete from session;
+delete from availability;
+delete from room;
+delete from person;
