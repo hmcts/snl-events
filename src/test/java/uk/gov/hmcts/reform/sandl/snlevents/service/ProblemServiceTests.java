fccf73eea47d1939a99fa8d6e7e13471193647aa
Szymon Filipczyk <s.filipczyk@kainos.com>
2018-07-09 17:48:46 +0200

    add ProblemServiceTests

---
+++ src/test/java/uk/gov/hmcts/reform/sandl/snlevents/service/ProblemServiceTests.java
@@ -0,0 +1,162 @@
+package uk.gov.hmcts.reform.sandl.snlevents.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.sandl.snlevents.model.db.Problem;
+import uk.gov.hmcts.reform.sandl.snlevents.model.db.ProblemReference;
+import uk.gov.hmcts.reform.sandl.snlevents.model.response.ProblemReferenceResponse;
+import uk.gov.hmcts.reform.sandl.snlevents.model.response.ProblemResponse;
+import uk.gov.hmcts.reform.sandl.snlevents.repository.db.ProblemRepository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+public class ProblemServiceTests {
+    public static final String ID = "id";
+    public static final String MESSAGE = "msg";
+    public static final String SEVERITY = "sev";
+    public static final String DESCRIPTION = "desc";
+    public static final String ENTITY = "ent";
+    public static final String ENTITY_ID = "eid";
+    public static final String PROBLEM_ID = "problem-id";
+
+    @TestConfiguration
+    static class ProblemServiceTestContextConfiguration {
+        @Bean
+        public ProblemService problemService() {
+            return new ProblemService();
+        }
+    }
+
+    @Autowired
+    private ProblemService problemService;
+
+    @MockBean
+    private ProblemRepository problemRepository;
+
+    @Test
+    public void getProblemsReturnsEmptyListWhenRepositoryReturnsEmptyList() {
+        when(problemRepository.findAll()).thenReturn(new ArrayList<>());
+
+        List<ProblemResponse> ret = problemService.getProblems();
+        assertThat(ret).isEqualTo(Collections.emptyList());
+    }
+
+    @Test
+    public void getProblemsReturnsProblemsFromRepository() {
+        when(problemRepository.findAll()).thenReturn(createProblems());
+
+        List<ProblemResponse> expectedProblemResponses = createProblemResponses();
+
+        List<ProblemResponse> problemResponses = problemService.getProblems();
+        assertThat(problemResponses).isEqualTo(expectedProblemResponses);
+    }
+
+    @Test
+    public void saveCallsSaveOnRepository() {
+        Problem problem = createProblem();
+        problemService.save(problem);
+
+        verify(problemRepository, times(1)).save(problem);
+    }
+
+    @Test
+    public void removeIfExistDeletesProblemsIfItExists() {
+        when(problemRepository.exists(PROBLEM_ID)).thenReturn(true);
+
+        problemService.removeIfExist(PROBLEM_ID);
+
+        verify(problemRepository, times(1)).delete(PROBLEM_ID);
+    }
+
+    @Test
+    public void removeIfExistDoesNothingWhenProblemDoesNotExist() {
+        when(problemRepository.exists(PROBLEM_ID)).thenReturn(false);
+
+        problemService.removeIfExist(PROBLEM_ID);
+
+        verify(problemRepository, times(0)).delete(PROBLEM_ID);
+    }
+
+    @Test
+    public void getProblemsByReferenceTypeIdReturnsProblemResponsesFromRepository() {
+        when(problemRepository.getProblemsByReferenceEntityId(ENTITY_ID)).thenReturn(createProblems());
+
+        // todo what is reference entity id and reference type id?
+        List<ProblemResponse> problems = problemService.getProblemsByReferenceTypeId(ENTITY_ID);
+        assertThat(problems).isEqualTo(createProblemResponses());
+    }
+
+    @Test
+    public void getProblemsByUserTransactionIdReturnsProblemResponsesFromRepository() {
+        UUID uuid = UUID.randomUUID();
+        when(problemRepository.getProblemsByUserTransactionId(uuid)).thenReturn(createProblems());
+
+        List<ProblemResponse> problems = problemService.getProblemsByUserTransactionId(uuid);
+        assertThat(problems).isEqualTo(createProblemResponses());
+    }
+
+    private Problem createProblem() {
+        Problem problem = new Problem();
+        problem.setId(ID);
+        problem.setMessage(MESSAGE);
+        problem.setSeverity(SEVERITY);
+
+        ProblemReference problemReference = new ProblemReference();
+        problemReference.setDescription(DESCRIPTION);
+        problemReference.setEntity(ENTITY);
+        problemReference.setEntityId(ENTITY_ID);
+
+        List<ProblemReference> problemReferences = new ArrayList<>();
+        problemReferences.add(problemReference);
+
+        problem.setReferences(problemReferences);
+
+        return problem;
+    }
+
+    private List<Problem> createProblems() {
+        List<Problem> problems = new ArrayList<>();
+        problems.add(createProblem());
+
+        return problems;
+    }
+
+    private ProblemResponse createProblemResponse() {
+        ProblemResponse problemResponse = new ProblemResponse();
+        problemResponse.setId(ID);
+        problemResponse.setMessage(MESSAGE);
+        problemResponse.setSeverity(SEVERITY);
+
+        ProblemReferenceResponse problemReference = new ProblemReferenceResponse();
+        problemReference.setDescription(DESCRIPTION);
+        problemReference.setEntity(ENTITY);
+        problemReference.setEntityId(ENTITY_ID);
+
+        List<ProblemReferenceResponse> problemReferences = new ArrayList<>();
+        problemReferences.add(problemReference);
+        problemResponse.setReferences(problemReferences);
+
+        return problemResponse;
+    }
+
+    private List<ProblemResponse> createProblemResponses() {
+        List<ProblemResponse> responses = new ArrayList<>();
+        responses.add(createProblemResponse());
+
+        return responses;
+    }
+}
