1b2458e5d38b144b01a6b49c2510e14cd369d3f1
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2018-07-17 10:19:30 +0200

    checkstyle fix

--- src/test/java/uk/gov/hmcts/reform/sandl/snlevents/service/FactMessageServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sandl/snlevents/service/FactMessageServiceTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.sandl.snlevents.service;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -10,10 +9,8 @@ import uk.gov.hmcts.reform.sandl.snlevents.model.db.Problem;
 
 import java.io.IOException;
 import java.util.UUID;
-import java.util.function.Function;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
