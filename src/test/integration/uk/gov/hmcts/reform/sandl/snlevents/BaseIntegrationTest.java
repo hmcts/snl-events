24c48728fad7340b099c2c1cfbc87f3edac32659
radoslawlandowskikainos <r.landowski@kainos.com>
2018-07-23 13:55:57 +0200

    fix config

--- src/test/integration/uk/gov/hmcts/reform/sandl/snlevents/BaseIntegrationTest.java
+++ src/test/integration/uk/gov/hmcts/reform/sandl/snlevents/BaseIntegrationTest.java
@@ -6,7 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    classes = Application.class)
 public abstract class BaseIntegrationTest {
 
     @Before
