939dbaffb67916b88fa5be6d3f4ffca85e53cc6d
Kamil Pakur <k.pakur@kainos.com>
2018-07-24 16:42:36 +0200

    SL-998 adding two vars

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -20,6 +20,16 @@ variable "env" {
 
 variable "ilbIp" {}
 
+variable "tenant_id" {
+  type                  = "string"
+  description           = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
+}
+
+variable "jenkins_AAD_objectId" {
+  type                        = "string"
+  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
 variable "external_host_name" {
   default = "snl-events.reform.hmcts.net"
 }
