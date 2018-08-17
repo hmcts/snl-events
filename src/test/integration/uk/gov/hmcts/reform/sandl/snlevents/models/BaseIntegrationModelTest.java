d67f0bdc81e505a80f926dd6bef823353ba92e90
Kamil Zielinski <K.Zielinski@kainos.com>
2018-08-17 13:59:55 +0200

    SL-1348 SL-1349: added test base class for model tests

---
+++ src/test/integration/uk/gov/hmcts/reform/sandl/snlevents/models/BaseIntegrationModelTest.java
@@ -0,0 +1,10 @@
+package uk.gov.hmcts.reform.sandl.snlevents.models;
+
+import uk.gov.hmcts.reform.sandl.snlevents.fakerules.BaseIntegrationTestWithFakeRules;
+
+public abstract class BaseIntegrationModelTest extends BaseIntegrationTestWithFakeRules {
+    static final String MAIN_TYPE_CODE = "main_type_code1";
+    static final String MAIN_TYPE_DESCRIPTION = "main_type_desc1";
+    static final String REF_TYPE_CODE = "code1";
+    static final String REF_TYPE_DESCRIPTION = "desc1";
+}
