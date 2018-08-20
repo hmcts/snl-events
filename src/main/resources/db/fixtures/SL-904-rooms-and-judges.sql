ad20d68ae0f33420cb9e5499ca6474b0f35650cf
Kamil Zielinski <K.Zielinski@kainos.com>
2018-08-20 12:19:25 +0200

    SL-1348 SL-1349: updated test data. added room_type_code for rooms

--- src/main/resources/db/fixtures/SL-904-rooms-and-judges.sql
+++ src/main/resources/db/fixtures/SL-904-rooms-and-judges.sql
@@ -1,11 +1,11 @@
 -- rooms
-insert into room (id, name, room_type_code) values ('dc4ce4dc-b24f-4b46-91ab-014d4a28469b', 'Court 1', 'dev_room');
-insert into room (id, name, room_type_code) values ('8d269688-064c-41b6-a0a6-8ae4033a23e1', 'Court 2', 'dev_room');
-insert into room (id, name, room_type_code) values ('c73df533-f067-4535-935c-d798c833ea33', 'Court 3', 'dev_room');
-insert into room (id, name, room_type_code) values ('ed613c62-ea50-4de5-ad9a-07f46dac3f11', 'Court 4', 'dev_room');
-insert into room (id, name, room_type_code) values ('fb878f53-a2dc-4cfc-aac1-ca1a1c697a8e', 'Court 5', 'dev_room');
-insert into room (id, name, room_type_code) values ('c8c5554c-cd8d-42ca-8503-7eae334e255e', 'Court 6', 'dev_room');
-insert into room (id, name, room_type_code) values ('b08d0570-1759-412d-8141-d8e1fd46de30', 'Court 7', 'dev_room');
+insert into room (id, name, room_type_code) values ('dc4ce4dc-b24f-4b46-91ab-014d4a28469b', 'Court 1', 'court_room');
+insert into room (id, name, room_type_code) values ('8d269688-064c-41b6-a0a6-8ae4033a23e1', 'Court 2', 'court_room');
+insert into room (id, name, room_type_code) values ('c73df533-f067-4535-935c-d798c833ea33', 'Court 3', 'court_room');
+insert into room (id, name, room_type_code) values ('ed613c62-ea50-4de5-ad9a-07f46dac3f11', 'Court 4', 'court_room');
+insert into room (id, name, room_type_code) values ('fb878f53-a2dc-4cfc-aac1-ca1a1c697a8e', 'Court 5', 'court_room');
+insert into room (id, name, room_type_code) values ('c8c5554c-cd8d-42ca-8503-7eae334e255e', 'Court 6', 'court_room');
+insert into room (id, name, room_type_code) values ('b08d0570-1759-412d-8141-d8e1fd46de30', 'Court 7', 'court_room');
 
 -- judges
 insert into person values ('6ce3aa58-6986-4ed7-8c61-94fa2af73afb', 'JUDGE', 'DJ Cope', 'djcope');
