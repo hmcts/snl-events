9ad7af6e4e1a7cf6174fc8b792e65b09af147376
Kamil Zielinski <K.Zielinski@kainos.com>
2018-08-22 14:41:59 +0200

    SL-1348 SL-1349: use ReferenceData class in BaseIngerationModelTest

--- src/test/integration/uk/gov/hmcts/reform/sandl/snlevents/models/BaseIntegrationModelTest.java
+++ src/test/integration/uk/gov/hmcts/reform/sandl/snlevents/models/BaseIntegrationModelTest.java
@@ -1,10 +1,11 @@
 package uk.gov.hmcts.reform.sandl.snlevents.models;
 
+import uk.gov.hmcts.reform.sandl.snlevents.common.ReferenceData;
 import uk.gov.hmcts.reform.sandl.snlevents.fakerules.BaseIntegrationTestWithFakeRules;
 
 public abstract class BaseIntegrationModelTest extends BaseIntegrationTestWithFakeRules {
-    static final String MAIN_TYPE_CODE = "main_type_code1";
-    static final String MAIN_TYPE_DESCRIPTION = "main_type_desc1";
-    static final String REF_TYPE_CODE = "code1";
-    static final String REF_TYPE_DESCRIPTION = "desc1";
+    static final String MAIN_TYPE_CODE = ReferenceData.MAIN_TYPE_CODE;
+    static final String MAIN_TYPE_DESCRIPTION = ReferenceData.MAIN_TYPE_DESCRIPTION;
+    static final String REF_TYPE_CODE = ReferenceData.REF_TYPE_CODE;
+    static final String REF_TYPE_DESCRIPTION = ReferenceData.REF_TYPE_DESCRIPTION;
 }
