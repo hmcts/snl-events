ff30ce553b55fae0d0dfa73d17be121450b165da
Kamil Zielinski <K.Zielinski@kainos.com>
2018-08-22 14:40:58 +0200

    SL-1348 SL-1349: added ReferenceData into common module

--- src/test/common/uk/gov/hmcts/reform/sandl/snlevents/common/ReferenceData.java
+++ src/test/common/uk/gov/hmcts/reform/sandl/snlevents/common/ReferenceData.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.sandl.snlevents.common;
 
-public abstract class ReferenceDataBase {
+public abstract class ReferenceData {
     public static final String MAIN_TYPE_CODE = "main_type_code1";
     public static final String MAIN_TYPE_DESCRIPTION = "main_type_desc1";
     public static final String REF_TYPE_CODE = "code1";
