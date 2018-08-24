0ad17f217355d3d40fe87e482054ac9ab1c3b8a5
radoslawlandowskikainos <r.landowski@kainos.com>
2018-08-24 12:34:19 +0200

    SL-1472: Rename

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/actions/Action.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/actions/Action.java
@@ -1,13 +1,11 @@
 package uk.gov.hmcts.reform.sandl.snlevents.actions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.reform.sandl.snlevents.actions.interfaces.IAction;
-import uk.gov.hmcts.reform.sandl.snlevents.actions.interfaces.RulesProcessable;
-import uk.gov.hmcts.reform.sandl.snlevents.actions.interfaces.Transactionable;
+import uk.gov.hmcts.reform.sandl.snlevents.actions.interfaces.UserTransactionable;
 import uk.gov.hmcts.reform.sandl.snlevents.mappers.FactsMapper;
 
-public abstract class Action implements IAction, Transactionable {
+public abstract class Action implements IAction, UserTransactionable {
 
     protected FactsMapper factsMapper = new FactsMapper();
 
--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/actions/interfaces/UserTransactionable.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/actions/interfaces/UserTransactionable.java
@@ -5,7 +5,7 @@ import uk.gov.hmcts.reform.sandl.snlevents.model.db.UserTransactionData;
 import java.util.List;
 import java.util.UUID;
 
-public interface Transactionable {
+public interface UserTransactionable {
     List<UserTransactionData> generateUserTransactionData() throws Exception;
     UUID getUserTransactionId();
     UUID[] getAssociatedEntitiesIds();
