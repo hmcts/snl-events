55b6faa9ba66d9933582971bff110bdfe1053c0e
Kamil Zielinski <K.Zielinski@kainos.com>
2018-09-20 10:08:43 +0200

    SL-1516: set date format to ISO8601 in objectMapper

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/config/JacksonConfig.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/config/JacksonConfig.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.sandl.snlevents.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +19,7 @@ public class JacksonConfig {
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.findAndRegisterModules();
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.setDateFormat(new ISO8601DateFormat());
 
         return objectMapper;
     }
