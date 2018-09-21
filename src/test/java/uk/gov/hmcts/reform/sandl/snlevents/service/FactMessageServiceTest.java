93dd64c3a6dd65f398e3c2e74830105d37d410cf
Kamil Zielinski <K.Zielinski@kainos.com>
2018-09-21 08:10:47 +0200

    SL-1516: removed unused import

--- src/test/java/uk/gov/hmcts/reform/sandl/snlevents/service/FactMessageServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sandl/snlevents/service/FactMessageServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.sandl.snlevents.model.db.Problem;
 
