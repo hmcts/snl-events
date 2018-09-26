357dfef831df94e63f9dd885f7056936e40f4c6d
radoslawlandowskikainos <r.landowski@kainos.com>
2018-09-26 09:48:37 +0200

    SL-1608: set sesssionType to ftrack as it was before

--- src/main/resources/db/testdata/insert-testdata.sql
+++ src/main/resources/db/testdata/insert-testdata.sql
@@ -104,27 +104,27 @@ insert into person(id, person_type, name, username) values('a70f3975-379c-431c-b
 -- sessions
 insert into session (id, person_id, room_id, start, duration, session_type_code)
 values ('27ba6290-5068-4372-b831-8c01b06bbc34', 'b5bc80ec-8306-4f0f-8c6e-af218bb116c2',
-'30bcf571-45ca-4528-9d05-ce51b5e3fcde', '2018-04-24 14:30:00', 6000, 'small-claims');
+'30bcf571-45ca-4528-9d05-ce51b5e3fcde', '2018-04-24 14:30:00', 6000, 'fast-track');
 
 insert into session (id, person_id, room_id, start, duration, session_type_code)
 values ('947e7065-5867-40a8-88de-0d5a787cc301', 'b5bc80ec-8306-4f0f-8c6e-af218bb116c2',
-'30bcf571-45ca-4528-9d05-ce51b5e3fcde', '2018-04-24 12:30:00', 6000, 'small-claims');
+'30bcf571-45ca-4528-9d05-ce51b5e3fcde', '2018-04-24 12:30:00', 6000, 'fast-track');
 
 insert into session (id, person_id, room_id, start, duration, session_type_code)
 values ('7fd158b2-1025-49ca-bc82-09f4c60e2672', 'b5bc80ec-8306-4f0f-8c6e-af218bb116c2',
-'30bcf571-45ca-4528-9d05-ce51b5e3fcde', '2018-04-25 11:30:00', 6000, 'small-claims');
+'30bcf571-45ca-4528-9d05-ce51b5e3fcde', '2018-04-25 11:30:00', 6000, 'fast-track');
 
 insert into session (id, person_id, room_id, start, duration, session_type_code)
 values ('22299479-90e6-4fdf-8232-456056899f12', null,
-'3e699f29-6ea9-46bf-a338-00622fe0ae1b', '2018-04-26 11:30:00', 6000, 'small-claims');
+'3e699f29-6ea9-46bf-a338-00622fe0ae1b', '2018-04-26 11:30:00', 6000, 'fast-track');
 
 insert into session (id, person_id, room_id, start, duration, session_type_code)
 values ('817f50ea-31e2-4d02-bf13-9ffd866b7667', null,
-null, '2018-04-26 11:30:00', 6000, 'small-claims');
+null, '2018-04-26 11:30:00', 6000, 'fast-track');
 
 insert into session (id, person_id, room_id, start, duration, session_type_code)
 values ('26571383-0a2f-434f-9971-b230f9364748' ,'8d65caf8-1337-4938-8f98-5f6e37e0787a',
-null, '2018-04-26 11:30:00', 6000, 'small-claims');
+null, '2018-04-26 11:30:00', 6000, 'fast-track');
 
 -- hearings
 INSERT INTO hearing_part (id, case_number, case_title, case_type, hearing_type, duration, schedule_start, schedule_end, session_id, start, created_at, created_by)
