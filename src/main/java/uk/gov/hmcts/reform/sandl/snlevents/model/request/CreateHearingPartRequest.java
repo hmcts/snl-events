0599b090f504288609afa84aa4916fc34b4949fd
Kamil Zielinski <K.Zielinski@kainos.com>
2018-09-28 16:05:54 +0200

    SL-1535: rename CreateHearingPart to CreateHearingPartRequest

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/request/CreateHearingPartRequest.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/request/CreateHearingPartRequest.java
@@ -18,7 +18,7 @@ import javax.validation.constraints.Size;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreateHearingPart implements UserTransactional {
+public class CreateHearingPartRequest implements UserTransactional {
 
     @NotNull
     private UUID id;
@@ -32,11 +32,11 @@ public class CreateHearingPart implements UserTransactional {
     private String caseTitle;
 
     @NotBlank
-    @Size(max = 100)
-    private String caseType;
+    @Size(max = 255)
+    private String caseTypeCode;
 
-    @Size(max = 100)
-    private String hearingType;
+    @Size(max = 255)
+    private String hearingTypeCode;
 
     @NotNull
     @MinDuration(minMinutes = 1)
