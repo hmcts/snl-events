35675250f204c85aa495c3490f333c39768e9dcd
Kamil Zielinski <K.Zielinski@kainos.com>
2018-09-28 16:19:35 +0200

    SL-1535: formatted file

--- src/test/integration/uk/gov/hmcts/reform/sandl/snlevents/fakerules/repository/SessionRepositoryTest.java
+++ src/test/integration/uk/gov/hmcts/reform/sandl/snlevents/fakerules/repository/SessionRepositoryTest.java
@@ -40,7 +40,6 @@ public class SessionRepositoryTest extends BaseIntegrationTestWithFakeRules {
 
     @Test
     public void sessions_createdSessionIsRetrievable() {
-
         Person djCope = getJudgeFromDb(JUDGE_USER_NAME);
 
         Session session = createSession(djCope, january2018());
