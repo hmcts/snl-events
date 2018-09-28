3d209e7c7253e02e029fb4fd91aa8c3950eef68c
Kamil Zielinski <K.Zielinski@kainos.com>
2018-09-28 15:52:14 +0200

    SL-1535: added HearingPartResponse model

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/response/HearingPartResponse.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/response/HearingPartResponse.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.sandl.snlevents.model.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.val;
 import uk.gov.hmcts.reform.sandl.snlevents.model.Priority;
 import uk.gov.hmcts.reform.sandl.snlevents.model.db.HearingPart;
 
@@ -18,34 +19,32 @@ public class HearingPartResponse {
     private UUID id;
     private String caseNumber;
     private String caseTitle;
-    private String caseType; // TODO change to case type Code
-    private String communicationFacilitator;
-    private OffsetDateTime createdAt;
-    private Boolean deleted;
+    private String caseTypeCode;
+    private String hearingTypeCode;
     private Duration duration;
-    private String hearingType; // TODO change it to hearing Type Code
-    private Priority priority;
-    private UUID reservedJudgeId;
     private OffsetDateTime scheduleStart;
     private OffsetDateTime scheduleEnd;
-    private UUID session; // TODO change to sessionId
+    private Priority priority;
+    private UUID reservedJudgeId;
+    private String communicationFacilitator;
+    private Boolean deleted;
+    private UUID sessionId;
     private Long version;
 
     public HearingPartResponse(HearingPart hearingPart) {
         this.setId(hearingPart.getId());
         this.setCaseNumber(hearingPart.getCaseNumber());
         this.setCaseTitle(hearingPart.getCaseTitle());
-        this.setCaseType(hearingPart.getCaseType());
-        this.setCommunicationFacilitator(hearingPart.getCommunicationFacilitator());
-        this.setCreatedAt(hearingPart.getCreatedAt());
-        this.setDeleted(hearingPart.isDeleted());
+        this.setCaseTypeCode(hearingPart.getCaseType().getCode());
+        this.setHearingTypeCode(hearingPart.getHearingType().getCode());
         this.setDuration(hearingPart.getDuration());
-        this.setHearingType(hearingPart.getHearingType().getCode());
-        this.setPriority(hearingPart.getPriority());
-        this.setReservedJudgeId(hearingPart.getReservedJudgeId());
         this.setScheduleStart(hearingPart.getScheduleStart());
         this.setScheduleEnd(hearingPart.getScheduleEnd());
-        Optional.ofNullable(hearingPart.getSession()).ifPresent(s -> this.setSession(s.getId()));
+        this.setPriority(hearingPart.getPriority());
+        this.setReservedJudgeId(hearingPart.getReservedJudgeId());
+        this.setCommunicationFacilitator(hearingPart.getCommunicationFacilitator());
+        this.setDeleted(hearingPart.isDeleted());
+        Optional.ofNullable(hearingPart.getSession()).ifPresent(s -> this.setSessionId(s.getId()));
         this.setVersion(hearingPart.getVersion());
     }
 }
