54d9b1452688dcd69f5efcbf9e0549322c44b7e4
Kamil Zielinski <K.Zielinski@kainos.com>
2018-09-28 16:08:12 +0200

    SL-1535: added migration to add case_type_code and remove case_type = the same for audit table

--- src/main/resources/db/fixtures/SL-1535-migrate-hearing-type-to-hearing-type-code.sql
+++ src/main/resources/db/fixtures/SL-1535-migrate-hearing-type-to-hearing-type-code.sql
@@ -9,3 +9,16 @@ UPDATE hearing_part SET hearing_type_code = (SELECT hearing_type FROM hearing_pa
 
 -- Copy value of hearing type audit to new column
 UPDATE hearing_part_aud SET hearing_type_code = hearing_type;
+
+
+-- Added into case_type table case_types that were used previously as string
+INSERT INTO case_type (description, code) (SELECT DISTINCT case_type, case_type FROM hearing_part) ON CONFLICT DO NOTHING;
+
+-- Copy value of case type to case type code
+UPDATE hearing_part SET case_type_code = case_type;
+
+-- Set default hearing type code for these that has hearing type == NULL
+UPDATE hearing_part SET case_type_code = (SELECT case_type FROM hearing_part GROUP BY case_type ORDER BY COUNT(*) DESC LIMIT 1) WHERE case_type_code = NULL;
+
+-- Copy value of case type audit to new column
+UPDATE hearing_part_aud SET case_type_code = case_type;
