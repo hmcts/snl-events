3a69cc0cb0c2ac42f6f76b6489eff40ae7558d4b
Kamil Zielinski <K.Zielinski@kainos.com>
2018-09-28 16:15:08 +0200

    SL-1535: changed type of hearingTypes to hearingPartsResponse for SessionWithHearings model

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/response/SessionWithHearings.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/response/SessionWithHearings.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.sandl.snlevents.model.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.sandl.snlevents.model.db.HearingPart;
 
 import java.io.Serializable;
 import java.util.List;
@@ -15,5 +14,5 @@ public class SessionWithHearings implements Serializable {
 
     private List<SessionInfo> sessions;
 
-    private List<HearingPart> hearingParts;
+    private List<HearingPartResponse> hearingPartsResponse;
 }
