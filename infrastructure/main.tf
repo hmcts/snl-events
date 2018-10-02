a3a6fd83695c120b162ff87d88fcfdff804bbb17
Kamil Pakur <k.pakur@kainos.com>
2018-10-02 12:26:39 +0200

    SL-1722 trying to fix the issue with app_settings in piepline

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -47,7 +47,7 @@ module "snl-events" {
 
     SNL_RULES_URL = "${local.rules_url}"
 
-    SNL_S2S_JWT_SECRET = "${data.azurerm_key_vault_secret.s2s_jwt_secret.value}"
+    SNL_S2S_JWT_SECRET = "abc"
   }
 }
 
