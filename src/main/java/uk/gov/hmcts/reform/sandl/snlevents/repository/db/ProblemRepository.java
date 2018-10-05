54cae11561153b9e73f5f44c26f1ec073dbf174c
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-05 12:25:51 +0200

    made ProblemRepository pagable + refactor getAllSortedBySeverityAndCreatedAt method

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/repository/db/ProblemRepository.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/repository/db/ProblemRepository.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.reform.sandl.snlevents.repository.db;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.sandl.snlevents.model.db.Problem;
@@ -10,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface ProblemRepository extends JpaRepository<Problem, String> {
+public interface ProblemRepository extends PagingAndSortingRepository<Problem, String> {
 
     @Query("SELECT problem FROM Problem problem "
         + "ORDER BY CASE severity "
@@ -19,7 +21,7 @@ public interface ProblemRepository extends JpaRepository<Problem, String> {
         + "         WHEN 'Warning'  THEN '2' "
         + "         ELSE '2' "
         + "         END, created_at desc")
-    List<Problem> getAllSortedBySeverityAndCreatedAt();
+    Page<Problem> getAllSortedBySeverityAndCreatedAt(Pageable pageable);
 
     @Query("SELECT problem "
         + "FROM Problem problem LEFT OUTER JOIN problem.references as pr "
