ecb4432fec0e60a577d3b6c88b582d361cceda8f
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-10 09:59:07 +0200

    SL-1850: fixed linting issue

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/validation/annotations/NullOrNotBlank.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/validation/annotations/NullOrNotBlank.java
@@ -2,12 +2,12 @@ package uk.gov.hmcts.reform.sandl.snlevents.validation.annotations;
 
 import uk.gov.hmcts.reform.sandl.snlevents.validation.validators.NullOrNotBlankValidator;
 
-import javax.validation.Constraint;
-import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
 
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
