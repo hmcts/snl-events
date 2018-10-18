bf659c34763ecdf5b22e12d4923b44b97166ba89
Kamil Pakur <k.pakur@kainos.com>
2018-10-18 16:02:05 +0200

    SL-1551 exists subquery

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/request/search/HearingSpecification.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/request/search/HearingSpecification.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import uk.gov.hmcts.reform.sandl.snlevents.model.db.CaseType;
 import uk.gov.hmcts.reform.sandl.snlevents.model.db.Hearing;
+import uk.gov.hmcts.reform.sandl.snlevents.model.db.HearingPart;
+import uk.gov.hmcts.reform.sandl.snlevents.model.db.HearingPart_;
+import uk.gov.hmcts.reform.sandl.snlevents.model.db.Hearing_;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,21 +25,23 @@ public class HearingSpecification implements Specification<Hearing> {
     @Override
     public Predicate toPredicate(Root<Hearing> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
         ComparisonOperations operation = criteria.getOperation();
-        if (operation.equals(ComparisonOperations.EQUALS)) {
+        if (criteria.getKey().equals("listingStatus")
+            && operation.equals(ComparisonOperations.EQUALS)) {
+            // TODO the missing listing subquery
+            Subquery<HearingPart> subquery = query.subquery(HearingPart.class);
+            Root<HearingPart> hpRoot = subquery.from(HearingPart.class);
+            subquery.select(hpRoot);
+            subquery.where(cb.equal(hpRoot.get("hearingId"), root),cb.isNotNull(hpRoot.get("sessionId")));
+            //cb.and(subquery, );
+            return cb.exists(subquery);
+            //throw new RuntimeException("Operation not implemented!");
+            //return cb.isNotNull(root.get("id"));
+        } else if (operation.equals(ComparisonOperations.EQUALS)) {
             return cb.equal(root.get(criteria.getKey()), criteria.getValue());
         } else if (operation.equals(ComparisonOperations.IN)) {
             return root.get(criteria.getKey()).in(getArrayValues(criteria.getKey()));
         } else if (operation.equals(ComparisonOperations.LIKE)) {
             return cb.like(root.get(criteria.getKey()), "%" + criteria.getValue().toString() + "%");
-        } else if (operation.equals(ComparisonOperations.LISTING_STATUS)) {
-            // TODO the missing listing subquery
-//            Subquery<HearingPart> subquery = query.subquery(HearingPart.class);
-//            Root<HearingPart> hpRoot = subquery.from(HearingPart.class);
-//            subquery.select(hpRoot.get(HearingPart_.hearingId).get(Hearing_.id)) //TODO: FIX
-//                .where(cb.isNotNull(hpRoot.get("sessionId")));
-//            return cb.exists(subquery);
-            //throw new RuntimeException("Operation not implemented!");
-            return cb.isNotNull(root.get("id"));
         } else {
             return cb.isNotNull(root.get("id"));
         }
