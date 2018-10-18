6f71d2442d9a66ec5b207e83acea99c745a7c224
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2018-10-18 11:10:02 +0200

    SL-1551 the working filter by specification without Listing subquery

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/request/search/HearingSpecification.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/request/search/HearingSpecification.java
@@ -2,16 +2,17 @@ package uk.gov.hmcts.reform.sandl.snlevents.model.request.search;
 
 import lombok.AllArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
+import uk.gov.hmcts.reform.sandl.snlevents.model.db.CaseType;
 import uk.gov.hmcts.reform.sandl.snlevents.model.db.Hearing;
-import uk.gov.hmcts.reform.sandl.snlevents.model.db.HearingPart;
-import uk.gov.hmcts.reform.sandl.snlevents.model.db.HearingPart_;
-import uk.gov.hmcts.reform.sandl.snlevents.model.db.Hearing_;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import javax.persistence.criteria.Subquery;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 @AllArgsConstructor
 public class HearingSpecification implements Specification<Hearing> {
@@ -23,17 +24,41 @@ public class HearingSpecification implements Specification<Hearing> {
         if (operation.equals(ComparisonOperations.EQUALS)) {
             return cb.equal(root.get(criteria.getKey()), criteria.getValue());
         } else if (operation.equals(ComparisonOperations.IN)) {
-            return root.get(criteria.getKey()).in(criteria.getValue());
+            return root.get(criteria.getKey()).in(getArrayValues(criteria.getKey()));
         } else if (operation.equals(ComparisonOperations.LIKE)) {
             return cb.like(root.get(criteria.getKey()), "%" + criteria.getValue().toString() + "%");
         } else if (operation.equals(ComparisonOperations.LISTING_STATUS)) {
-            Subquery<HearingPart> subquery = query.subquery(HearingPart.class);
-            Root<HearingPart> hpRoot = subquery.from(HearingPart.class);
-            subquery.select(hpRoot.get(HearingPart_.hearingId).get(Hearing_.id)) //TODO: FIX
-                .where(cb.isNotNull(hpRoot.get("sessionId")));
-            return cb.exists(subquery);
+            // TODO the missing listing subquery
+//            Subquery<HearingPart> subquery = query.subquery(HearingPart.class);
+//            Root<HearingPart> hpRoot = subquery.from(HearingPart.class);
+//            subquery.select(hpRoot.get(HearingPart_.hearingId).get(Hearing_.id)) //TODO: FIX
+//                .where(cb.isNotNull(hpRoot.get("sessionId")));
+//            return cb.exists(subquery);
+            throw new RuntimeException("Operation not implemented!");
         } else {
             throw new RuntimeException("Operation not implemented!");
         }
     }
+
+    //TODO below a quickfix/prototype need to implement for all the fields and cleanup
+    private Object getArrayValues(String key) {
+        if (criteria.getKey().equals("reservedJudgeId")) {
+            List<String> values = ((List<String>)criteria.getValue());
+            List<UUID> toReturn = new ArrayList<>();
+            for (String value : values) {
+                toReturn.add(UUID.fromString(value));
+            }
+            return toReturn;
+        } else if (criteria.getKey().equals("caseType")) {
+            List<String> values = ((List<String>)criteria.getValue());
+            List<CaseType> toReturn = new ArrayList<>();
+            for (String value : values) {
+                toReturn.add(new CaseType(value, ""));
+            }
+            return toReturn;
+        } else {
+            return criteria.getValue();
+        }
+    }
+
 }
