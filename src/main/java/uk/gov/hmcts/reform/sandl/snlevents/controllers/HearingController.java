a537d99bbd6ee489720b45b177fee669480db6cc
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-29 11:51:34 +0100

    SL-1761: added put (unlist) Hearing endpoint

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/controllers/HearingController.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/controllers/HearingController.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.sandl.snlevents.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,11 +10,17 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.sandl.snlevents.actions.Action;
+import uk.gov.hmcts.reform.sandl.snlevents.actions.hearing.UnlistHearingAction;
 import uk.gov.hmcts.reform.sandl.snlevents.model.db.UserTransaction;
 import uk.gov.hmcts.reform.sandl.snlevents.model.request.HearingSessionRelationship;
+import uk.gov.hmcts.reform.sandl.snlevents.model.request.UnlistHearingRequest;
 import uk.gov.hmcts.reform.sandl.snlevents.model.response.HearingInfo;
 import uk.gov.hmcts.reform.sandl.snlevents.model.response.HearingWithSessionsResponse;
+import uk.gov.hmcts.reform.sandl.snlevents.model.usertransaction.UserTransactional;
+import uk.gov.hmcts.reform.sandl.snlevents.repository.db.HearingPartRepository;
 import uk.gov.hmcts.reform.sandl.snlevents.repository.db.HearingRepository;
+import uk.gov.hmcts.reform.sandl.snlevents.service.ActionService;
 import uk.gov.hmcts.reform.sandl.snlevents.service.HearingPartService;
 
 import java.util.UUID;
@@ -28,7 +35,16 @@ public class HearingController {
     private HearingRepository hearingRepository;
 
     @Autowired
+    private HearingPartRepository hearingPartRepository;
+
+    @Autowired
     private HearingPartService hearingPartService;
+
+    @Autowired
+    private ActionService actionService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public HearingInfo getHearingById(@PathVariable("id") UUID id) {
@@ -46,6 +62,21 @@ public class HearingController {
         @RequestBody HearingSessionRelationship assignment) throws Exception {
 
         UserTransaction ut = hearingPartService.assignHearingToSessionWithTransaction(hearingId, assignment);
+
+        return ok(ut);
+    }
+
+    @PutMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity unlist(@RequestBody UnlistHearingRequest unlistHearingRequest) {
+        // Todo move to service?
+        Action action = new UnlistHearingAction(
+            unlistHearingRequest,
+            hearingRepository,
+            hearingPartRepository,
+            objectMapper
+        );
+
+        UserTransaction ut = actionService.execute(action);
 
         return ok(ut);
     }
