0b8dbf3e959720f38730268356e9762b6bd5ced6
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-29 11:50:04 +0100

    SL-1761: fixed warning

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/request/UpdateListingRequest.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/request/UpdateListingRequest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.sandl.snlevents.model.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
@@ -9,6 +10,7 @@ import javax.validation.constraints.NotNull;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class UpdateListingRequest extends CreateHearingRequest {
     @NotNull
     private Long version;
