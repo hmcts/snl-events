ebe87ac9b12af9529f0d6ff86fb3e66ff4c1ddaa
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-29 11:41:22 +0100

    SL-1761: added unlist hearing action

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/actions/hearing/UnlistHearingAction.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/actions/hearing/UnlistHearingAction.java
@@ -1,8 +1,8 @@
 package uk.gov.hmcts.reform.sandl.snlevents.actions.hearing;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.val;
-import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.reform.sandl.snlevents.actions.Action;
 import uk.gov.hmcts.reform.sandl.snlevents.actions.interfaces.RulesProcessable;
 import uk.gov.hmcts.reform.sandl.snlevents.messages.FactMessage;
@@ -11,34 +11,44 @@ import uk.gov.hmcts.reform.sandl.snlevents.model.db.HearingPart;
 import uk.gov.hmcts.reform.sandl.snlevents.model.db.Session;
 import uk.gov.hmcts.reform.sandl.snlevents.model.db.UserTransactionData;
 import uk.gov.hmcts.reform.sandl.snlevents.model.request.UnlistHearingRequest;
+import uk.gov.hmcts.reform.sandl.snlevents.model.request.VersionInfo;
 import uk.gov.hmcts.reform.sandl.snlevents.repository.db.HearingPartRepository;
 import uk.gov.hmcts.reform.sandl.snlevents.repository.db.HearingRepository;
 import uk.gov.hmcts.reform.sandl.snlevents.service.RulesService;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class UnlistHearingAction extends Action implements RulesProcessable {
 
-    protected UnlistHearingRequest unlistHearingRequest;
-    protected Hearing hearing;
-    protected List<HearingPart> hearingParts;
-    protected List<Session> sessions;
+    private UnlistHearingRequest unlistHearingRequest;
+    private Hearing hearing;
+    private List<HearingPart> hearingParts;
+    private List<Session> sessions;
 
-    protected HearingRepository hearingRepository;
-    protected HearingPartRepository hearingPartRepository;
+    private HearingRepository hearingRepository;
+    private HearingPartRepository hearingPartRepository;
+
+    // id & hearing part string
+    private Map<UUID, String> originalHearingParts;
 
     public UnlistHearingAction(
         UnlistHearingRequest unlistHearingRequest,
         HearingRepository hearingRepository,
-        HearingPartRepository hearingPartRepository) {
+        HearingPartRepository hearingPartRepository,
+        ObjectMapper objectMapper) {
             this.unlistHearingRequest = unlistHearingRequest;
             this.hearingRepository = hearingRepository;
             this.hearingPartRepository = hearingPartRepository;
+            this.objectMapper = objectMapper;
     }
 
     @Override
@@ -46,21 +56,18 @@ public class UnlistHearingAction extends Action implements RulesProcessable {
         hearing = hearingRepository.findOne(unlistHearingRequest.getHearingId());
         hearingParts = hearing.getHearingParts();
         sessions = hearingParts.stream()
-            .map(hp -> hp.getSession())
+            .map(HearingPart::getSession)
             .filter(Objects::nonNull)
             .collect(Collectors.toList());
-
-        if (sessions.isEmpty()) {
-            throw new RuntimeException("Hearing parts assigned to Hearing haven't been listed yet");
-        }
+        // Validation moved to act() due to conflict with optimistic lock
     }
 
     @Override
     public UUID[] getAssociatedEntitiesIds() {
-        List<UUID> ids = Arrays.asList(unlistHearingRequest.getHearingId());
-        val hearingPartsIds = hearing.getHearingParts().stream().map(hp -> hp.getId()).collect(Collectors.toList());
+        List<UUID> ids = new LinkedList<>(Arrays.asList(unlistHearingRequest.getHearingId()));
+        val hearingPartsIds = hearing.getHearingParts().stream().map(HearingPart::getId).collect(Collectors.toList());
         val sessionsIds = hearing.getHearingParts().stream()
-            .map(hp -> hp.getSessionId())
+            .map(HearingPart::getSessionId)
             .filter(Objects::nonNull)
             .collect(Collectors.toList());
 
@@ -72,29 +79,45 @@ public class UnlistHearingAction extends Action implements RulesProcessable {
 
     @Override
     public void act() {
+        if (sessions.isEmpty()) {
+            throw new RuntimeException("Hearing parts assigned to Hearing haven't been listed yet");
+        }
+
+        originalHearingParts = mapHearingPartsToStrings(hearingParts);
+
         hearingParts.stream().forEach(hp -> {
             hp.setSession(null);
+            hp.setSessionId(null);
             hp.setStart(null);
+            VersionInfo vi = getVersionInfo(hp);
+            hp.setVersion(vi.getVersion());
         });
 
         hearingPartRepository.save(hearingParts);
     }
 
+    private VersionInfo getVersionInfo(HearingPart hp) {
+        Optional<VersionInfo> hpvi = unlistHearingRequest.getHearingPartsVersions()
+            .stream()
+            .filter(hpv -> hpv.getId().equals(hp.getId()))
+            .findFirst();
+        return hpvi.orElseThrow(() ->
+            new RuntimeException("Couldn't find version for hearing part with id " + hp.getId().toString())
+        );
+    }
+
     @Override
     public List<UserTransactionData> generateUserTransactionData() {
         List<UserTransactionData> userTransactionDataList = new ArrayList<>();
-
-        hearingParts.stream().forEach(hp -> {
-            try {
-                userTransactionDataList.add(new UserTransactionData("hearingPart",
-                    hp.getId(),
-                    objectMapper.writeValueAsString(hp),
-                    "update",
-                    "update",
-                    0)
-                );
-            } catch (Exception ex) { throw new RuntimeException(ex); }
-        });
+        originalHearingParts.forEach((id, hpString) ->
+            userTransactionDataList.add(new UserTransactionData("hearingPart",
+                id,
+                hpString,
+                "update",
+                "update",
+                0)
+            )
+        );
 
         userTransactionDataList.add(getLockedEntityTransactionData("hearing", hearing.getId()));
         sessions.stream().forEach(s ->
@@ -130,4 +153,17 @@ public class UnlistHearingAction extends Action implements RulesProcessable {
         return new UserTransactionData(entity, id, null, "lock", "unlock", 0);
     }
 
+    private Map<UUID, String> mapHearingPartsToStrings(List<HearingPart> hearingParts) {
+        Map<UUID, String> originalIdStringPair = new HashMap<UUID, String>();
+        hearingParts.stream().forEach(hp -> {
+            try {
+                String hearingPartString = objectMapper.writeValueAsString(hp);
+                originalIdStringPair.put(hp.getId(), hearingPartString);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return originalIdStringPair;
+    }
 }
