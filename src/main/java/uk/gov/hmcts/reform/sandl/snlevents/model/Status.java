88d101a6557591b25e2bfe273e093b3fe16f917c
Remigiusz Kruk <R.Kruk@kainos.com>
2018-11-06 08:45:16 +0100

    SL-1967: Revert "SL-1967 StatusConfig status as enum and related service initial version"
    This reverts commit 34cc4bd608312ba6774ecd9f7ce856a19aec6654.

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/Status.java
+++
@@ -1,6 +0,0 @@
-package uk.gov.hmcts.reform.sandl.snlevents.model;
-
-public enum Status {
-    Unlisted,
-    Listed;
-}
--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/db/StatusConfig.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/db/StatusConfig.java
@@ -1,14 +1,11 @@
 package uk.gov.hmcts.reform.sandl.snlevents.model.db;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.sandl.snlevents.model.Status;
 
 import java.io.Serializable;
+
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.Id;
 
 @Entity
@@ -17,9 +14,7 @@ import javax.persistence.Id;
 public class StatusConfig implements Serializable {
 
     @Id
-    @Getter
-    @Enumerated(EnumType.STRING)
-    private Status status;
+    private String status;
 
     private boolean canBeListed;
 
