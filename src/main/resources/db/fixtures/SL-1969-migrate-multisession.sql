85e06db2866a9bb457055695f875d6be457cf900
Andrew Taylor <a.taylor@kainos.com>
2018-11-06 16:24:16 +0100

    removed default values from number_of_sessions and removed migration code for hearing_aud as to not pollute history

--- src/main/resources/db/fixtures/SL-1969-migrate-multisession.sql
+++
@@ -1,2 +0,0 @@
-update hearing set is_multisession = CASE WHEN duration >= 86400000 THEN true ELSE false END;
-update hearing_aud set is_multisession = CASE WHEN duration >= 86400000 THEN true ELSE false END;
