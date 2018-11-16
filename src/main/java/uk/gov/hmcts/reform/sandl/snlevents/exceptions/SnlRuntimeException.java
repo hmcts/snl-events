dc1e575eb0eb11924c4a8b0fa2fdea36fcd312c1
Kamil Zielinski <K.Zielinski@kainos.com>
2018-11-16 10:59:07 +0100

    SL-SL-1: extended SNLRuntimeException

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/exceptions/SnlRuntimeException.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/exceptions/SnlRuntimeException.java
@@ -4,4 +4,8 @@ public class SnlRuntimeException extends RuntimeException {
     public SnlRuntimeException(Throwable cause) {
         super(cause);
     }
+
+    public SnlRuntimeException(String cause) {
+        super(cause);
+    }
 }
