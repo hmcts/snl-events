248c311282debfe77c81d9602d14a804c83dbd30
Remigiusz Kruk <R.Kruk@kainos.com>
2018-11-22 11:32:40 +0100

    SL-1900: change key naming in response for possibleActions

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/response/PossibleActions.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/response/PossibleActions.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.reform.sandl.snlevents.model.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class PossibleActions {
 
+    @JsonProperty(value = "Unlist")
     private boolean unlist;
 
+    @JsonProperty(value = "Withdraw")
     private boolean withdraw;
 
+    @JsonProperty(value = "Adjourn")
     private boolean adjourn;
 }
