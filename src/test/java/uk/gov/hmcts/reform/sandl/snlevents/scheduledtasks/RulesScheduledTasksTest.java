a9289c80f25d2443a123acd05f84976991fc76a3
Kamil Pakur <k.pakur@kainos.com>
2018-11-22 15:16:14 +0100

    tests improved

--- src/test/java/uk/gov/hmcts/reform/sandl/snlevents/scheduledtasks/RulesScheduledTasksTest.java
+++ src/test/java/uk/gov/hmcts/reform/sandl/snlevents/scheduledtasks/RulesScheduledTasksTest.java
@@ -5,11 +5,14 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.ResourceAccessException;
 import uk.gov.hmcts.reform.sandl.snlevents.service.ReloadRulesService;
 
 import java.io.IOException;
+import java.net.ConnectException;
 
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -32,5 +35,32 @@ public class RulesScheduledTasksTest {
 
         verify(reloadRulesService, times(1)).reloadIfNeeded();
         verify(reloadRulesService, times(1)).reloadDateAndTimeIfNeeded();
+    }
+
+    @Test
+    public void setRulesdo_not_throw_ex_when_connection_errors() throws IOException {
+        doThrow(new ResourceAccessException("ex", new ConnectException()))
+            .when(reloadRulesService).reloadIfNeeded();
+        doThrow(new ResourceAccessException("ex", new ConnectException()))
+            .when(reloadRulesService).reloadDateAndTimeIfNeeded();
+
+        rulesScheduledTasks.reloadRulesFactsFromDb();
+        rulesScheduledTasks.setRulesDateAndTime();
+    }
+
+    @Test(expected = ResourceAccessException.class)
+    public void reloadRulesFactsFromDb_throw_non_connection_errors() throws IOException {
+        doThrow(new ResourceAccessException("ex"))
+            .when(reloadRulesService).reloadIfNeeded();
+
+        rulesScheduledTasks.reloadRulesFactsFromDb();
+    }
+
+    @Test(expected = ResourceAccessException.class)
+    public void setRulesDateAndTime_throw_non_connection_errors() throws IOException {
+        doThrow(new ResourceAccessException("ex"))
+            .when(reloadRulesService).reloadDateAndTimeIfNeeded();
+
+        rulesScheduledTasks.setRulesDateAndTime();
     }
 }
