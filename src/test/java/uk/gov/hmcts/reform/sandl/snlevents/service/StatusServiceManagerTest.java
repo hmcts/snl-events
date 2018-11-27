7e1cbb816ccc730a2ae7369bcd53eb91b5122a9f
Andrew Taylor <a.taylor@kainos.com>
2018-11-27 11:31:43 +0100

    [PREVIEW] Feature/sl 2050/withdraw listing request 2 (#195)
    * SL-1900: add common code for SL-1900 and SL-2050
    
    * SL-1900: fix tests
    
    * SL-1900: add start date of hearing to hearingWithSessionsResponse
    
    * SL-1900: add possibility to check if operations can be performed on backend
    
    * SL-1900: fix unlist check
    
    * SL-1900: remove unnecessary logic
    
    * BaseClass and tests. Added mockStatuses and isCanBeWithdrawn(hp) to statusServiceManager
    
    * Checkstyle and updated tests
    
    * Reverted code to not use inheritance
    
    * Checkstyle and new test
    
    * Renamed tests
    
    * Re-added try-catch
    
    * Updated StatusServiceManager tests
    
    * Removed getVersionInfo() and link between hearingPart and session
    
    * Checkstyle
    
    * Removed hearingPart status checking & introduced hearing version check
    
    * Added userTransactionData tests
    
    * Checkstyle
    
    * SonarQube issues
    
    * Checkstyle
    
    * Created static helper class for common code in actions
    
    * Made variables gloabl
    
    * SL-1900: change key names in possible actions response
    
    * Moved variable to parameter
    
    * Modular UserTransactionDataPreparerService
    
    * Hard coded actions in transactionDataPreparerService
    
    * removed no-sonar
    
    * Added status to hearingInfo and updated tests
    
    * added version to hearingsWithSessions response
    
    * Added NOSONAR comment for duplicated warning. Changed dateTime comparison to only date comparison
    
    * Added suppress warning and return true for withdraw unlisted
    
    * Removed suppress warning, renamed variable
    
    * Changed to predicate as per SonarQube
    
    * removed todo - sonarqube
    
    * Can withdraw if date = today's date
    
    * Trigger Jenkins

--- src/test/java/uk/gov/hmcts/reform/sandl/snlevents/service/StatusServiceManagerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sandl/snlevents/service/StatusServiceManagerTest.java
@@ -15,8 +15,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 public class StatusServiceManagerTest {
-
-    StatusServiceManager statusServiceManager = new StatusServiceManager();
+    private static final Long VERSION = 0L;
+    private StatusServiceManager statusServiceManager = new StatusServiceManager();
 
     @Test
     public void shouldGiveProperValues_whenEntityHasListedStatus() {
@@ -36,8 +36,30 @@ public class StatusServiceManagerTest {
         assertThat(statusServiceManager.shouldBeCountInUtilization(entity)).isEqualTo(false);
     }
 
+    @Test
+    public void canAdjournIsTrue_whenHearingHasListedStatus() {
+        Hearing hearing = createHearingWithStatus(createListedStatus());
+
+        assertThat(statusServiceManager.canBeAdjourned(hearing)).isEqualTo(true);
+    }
+
+    @Test
+    public void canAdjournIsFalse_whenHearingHasUnlistedStatus() {
+        Hearing hearing = createHearingWithStatus(createUnlistedStatus());
+
+        assertThat(statusServiceManager.canBeAdjourned(hearing)).isEqualTo(false);
+    }
+
+    @Test
+    public void canWithdrawIsFalse_whenHearingHasAdjournedStatus() {
+        Hearing hearing = createHearingWithStatus(createAdjournedStatus());
+
+        assertThat(statusServiceManager.canBeWithdrawn(hearing)).isEqualTo(false);
+    }
+
     private Hearing createHearingWithStatus(StatusConfig status) {
         val hearing = new Hearing();
+        hearing.setVersion(VERSION);
         hearing.setStatus(status);
 
         return hearing;
@@ -55,6 +77,9 @@ public class StatusServiceManagerTest {
         status.setStatus(Status.Listed);
         status.setCanBeListed(false);
         status.setCanBeUnlisted(true);
+        status.setCanBeAdjourned(true);
+        status.setCanBeVacated(true);
+        status.setCanBeWithdrawn(true);
         status.setCountInUtilization(true);
 
         return status;
@@ -65,6 +90,22 @@ public class StatusServiceManagerTest {
         status.setStatus(Status.Unlisted);
         status.setCanBeListed(true);
         status.setCanBeUnlisted(false);
+        status.setCanBeAdjourned(false);
+        status.setCanBeVacated(false);
+        status.setCanBeWithdrawn(true);
+        status.setCountInUtilization(false);
+
+        return status;
+    }
+
+    private StatusConfig createAdjournedStatus() {
+        val status = new StatusConfig();
+        status.setStatus(Status.Adjourned);
+        status.setCanBeListed(false);
+        status.setCanBeUnlisted(false);
+        status.setCanBeAdjourned(false);
+        status.setCanBeVacated(false);
+        status.setCanBeWithdrawn(false);
         status.setCountInUtilization(false);
 
         return status;
