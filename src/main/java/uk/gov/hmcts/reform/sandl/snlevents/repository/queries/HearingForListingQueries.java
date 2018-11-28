664f2fdb0067bd10bceea40f18bf4296f616154e
radoslawlandowskikainos <r.landowski@kainos.com>
2018-11-28 09:19:50 +0100

    SL-1971: uppercase SQl syntax

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/repository/queries/HearingForListingQueries.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/repository/queries/HearingForListingQueries.java
@@ -7,21 +7,21 @@ public class HearingForListingQueries {
 
     private final String hearingForListingSelectCount = "SELECT Count(*) ";
     private final String hearingForListingSelectHearings = "SELECT h.id, case_number, case_title, "
-        + "ct.code as case_type_code, "
-        + "ct.description as case_type_description, ht.code as hearing_type_code, "
-        + "ht.description as hearing_type_description, (duration * 1000000000) as duration, "
+        + "ct.code AS case_type_code, "
+        + "ct.description AS case_type_description, ht.code AS hearing_type_code, "
+        + "ht.description AS hearing_type_description, (duration * 1000000000) AS duration, "
         + "schedule_start, schedule_end, version, "
-        + "priority, communication_facilitator, reserved_judge_id, p.name as reserved_judge_name, number_of_sessions, "
+        + "priority, communication_facilitator, reserved_judge_id, p.name AS reserved_judge_name, number_of_sessions, "
         + "h.status, is_multisession ";
 
     private final String hearingForListingQueryBody = "FROM hearing h "
-        + "LEFT JOIN person p on p.id = h.reserved_judge_id "
-        + "INNER JOIN case_type ct on h.case_type_code = ct.code "
-        + "INNER JOIN hearing_type ht on h.hearing_type_code = ht.code "
-        + "INNER JOIN status_config sc on h.status = sc.status "
-        + "WHERE can_be_listed = true "
+        + "LEFT JOIN person p ON p.id = h.reserved_judge_id "
+        + "INNER JOIN case_type ct ON h.case_type_code = ct.code "
+        + "INNER JOIN hearing_type ht ON h.hearing_type_code = ht.code "
+        + "INNER JOIN status_config sc ON h.status = sc.status "
+        + "WHERE can_be_listed = TRUE "
         + "AND h.status != 'Listed' "
-        + "AND is_deleted = false ";
+        + "AND is_deleted = FALSE ";
 
     private String orderByQueryPart = "ORDER BY <order_property> <order_direction>";
 
