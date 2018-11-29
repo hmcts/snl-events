78953d7a3e60431889e521b80d9f60ac540597cc
radoslawlandowskikainos <r.landowski@kainos.com>
2018-11-29 11:49:24 +0100

    SL-1971: fix varaible names

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/repository/queries/HearingForListingQueries.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/repository/queries/HearingForListingQueries.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class HearingForListingQueries {
 
-    private final String hearingForListingSelectCount = "SELECT Count(*) ";
-    private final String hearingForListingSelectHearings = "SELECT h.id, case_number, case_title, "
+    private static final String HEARING_FOR_LISTING_SELECT_COUNT = "SELECT Count(*) ";
+    private static final String HEARING_FOR_LISTING_SELECT_ENTITIES = "SELECT h.id, case_number, case_title, "
         + "ct.code AS case_type_code, "
         + "ct.description AS case_type_description, ht.code AS hearing_type_code, "
         + "ht.description AS hearing_type_description, (duration * 1000000000) AS duration, "
@@ -15,7 +15,7 @@ public class HearingForListingQueries {
         + "priority, communication_facilitator, reserved_judge_id, p.name AS reserved_judge_name, number_of_sessions, "
         + "h.status, is_multisession ";
 
-    private final String hearingForListingQueryBody = "FROM hearing h "
+    private static final String HEARING_FOR_LISTING_QUERY_BODY = "FROM hearing h "
         + "LEFT JOIN person p ON p.id = h.reserved_judge_id "
         + "INNER JOIN case_type ct ON h.case_type_code = ct.code "
         + "INNER JOIN hearing_type ht ON h.hearing_type_code = ht.code "
@@ -24,23 +24,23 @@ public class HearingForListingQueries {
         + "AND h.status != 'Listed' "
         + "AND is_deleted = FALSE ";
 
-    private String orderByQueryPart = "ORDER BY <order_property> <order_direction>";
+    private static final String ORDER_BY_QUERY_PART = "ORDER BY <order_property> <order_direction>";
 
-    private final String orderPropertyPlaceholder = "<order_property>";
-    private final String orderDirectionPlaceholder = "<order_direction>";
-
-    private String getOrderByQueryPart(String property, String direction) {
-        return orderByQueryPart
-            .replace(orderPropertyPlaceholder, property)
-            .replace(orderDirectionPlaceholder, direction);
-    }
+    private static final String ORDER_PROPERTY_PLACEHOLDER = "<order_property>";
+    private static final String ORDER_DIRECTION_PLACEHOLDER = "<order_direction>";
 
     public String getMainQuery(HearingForListingColumn property, Sort.Direction direction) {
-        return hearingForListingSelectHearings + hearingForListingQueryBody
+        return HEARING_FOR_LISTING_SELECT_ENTITIES + HEARING_FOR_LISTING_QUERY_BODY
             + getOrderByQueryPart(property.getColumnName(), direction.toString());
     }
 
     public String getCountQuery() {
-        return hearingForListingSelectCount + hearingForListingQueryBody;
+        return HEARING_FOR_LISTING_SELECT_COUNT + HEARING_FOR_LISTING_QUERY_BODY;
+    }
+
+    private String getOrderByQueryPart(String property, String direction) {
+        return ORDER_BY_QUERY_PART
+            .replace(ORDER_PROPERTY_PLACEHOLDER, property)
+            .replace(ORDER_DIRECTION_PLACEHOLDER, direction);
     }
 }
