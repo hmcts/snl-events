efde2bd42f980a93dd23be64465778157b810d2f
Kamil Zielinski <K.Zielinski@kainos.com>
2018-11-30 08:56:41 +0100

    SL-1879: attempt 2 to fix sonar cloud issues

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/actions/hearing/VacateHearingAction.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/actions/hearing/VacateHearingAction.java
@@ -29,8 +29,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 
-@SuppressWarnings("common-java:DuplicatedBlocks")
-public class VacateHearingAction extends Action implements RulesProcessable {
+public class VacateHearingAction extends Action implements RulesProcessable { //NOSONAR
     protected VacateHearingRequest vacateHearingRequest;
     protected Hearing hearing;
     protected List<HearingPart> hearingParts;
