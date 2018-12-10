6dae35224df7b0ba87d19c9fe311ac770805782b
Kamil Zielinski <K.Zielinski@kainos.com>
2018-12-10 14:44:47 +0100

    SL-1794: added not null validators to delete listing req

--- src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/request/DeleteListingRequest.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlevents/model/request/DeleteListingRequest.java
@@ -5,16 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.sandl.snlevents.model.usertransaction.UserTransactional;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DeleteListingRequest implements UserTransactional {
-
+    @NotNull
     private UUID hearingId;
 
+    @NotNull
     private Long hearingVersion;
 
+    @NotNull
     private UUID userTransactionId;
 }
